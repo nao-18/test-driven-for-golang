d60b4f1cedbe1d91d8a2803774f50fd00eb5edb1
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-30 22:30:02 +0900

    other language support

--- src/hello_test.go
+++ src/hello_test.go
@@ -11,15 +11,27 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+	t.Run("saying hello to people for english", func(t *testing.T) {
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Christiane", "French")
+		want := "Bonjour, Christiane"
 		assertCorrectMessage(t, got, want)
 	})
 }
