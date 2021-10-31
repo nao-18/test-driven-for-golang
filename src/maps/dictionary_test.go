5709de2bd239909bef8fed5ac9da08633928e6f1
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 16:08:17 +0900

    add new word test and existing word test

--- src/maps/dictionary_test.go
+++ src/maps/dictionary_test.go
@@ -27,6 +27,12 @@ func assertError(t *testing.T, got, want error) {
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
 	}
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
+	}
 }
 
 func assertStrings(t *testing.T, got, want string) {
@@ -38,14 +44,28 @@ func assertStrings(t *testing.T, got, want string) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
 
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrNotFound)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
-
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
