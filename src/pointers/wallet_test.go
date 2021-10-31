fd38851245b5b70ab148e8f31afd26d5bf22d00f
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 14:53:52 +0900

    add error assertion

--- src/pointers/wallet_test.go
+++ src/pointers/wallet_test.go
@@ -15,6 +15,13 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitconin(10))
@@ -33,9 +40,6 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitconin(100))
 
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
+		assertError(t, err)
 	})
 }
