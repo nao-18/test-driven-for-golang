eed9ba6a3e52ce4a1528b9f5d88af00ec61168e9
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 13:47:36 +0900

    add printf

--- src/pointers/wallet_test.go
+++ src/pointers/wallet_test.go
@@ -1,11 +1,15 @@
 package wallet
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
 	want := 10
 
 	if got != want {
