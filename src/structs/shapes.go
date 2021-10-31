47ddc6464a385377e8ede7bd6f363248763f73b4
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 12:54:24 +0900

    fixed shapes.go and shapes_test.go

--- src/structs/shapes.go
+++ src/structs/shapes.go
@@ -1,5 +1,7 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
@@ -13,6 +15,10 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Redius * c.Redius
 }
