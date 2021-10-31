138367f05053df4c8ada7e64648ccfac4e494182
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 14:38:02 +0900

    add Withdraw test

--- src/pointers/wallet_test.go
+++ src/pointers/wallet_test.go
@@ -6,13 +6,28 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitconin(10))
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	want := Bitconin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitconin(10))
+		got := wallet.Balance()
+		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		want := Bitconin(10)
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitconin(20)}
+		wallet.Withdraw(Bitconin(10))
+		got := wallet.Balance()
+		want := Bitconin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
