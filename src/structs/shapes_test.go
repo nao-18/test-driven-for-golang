3a0e4510d109af92f054577046ac2c5c5bf83f5d
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 12:36:46 +0900

    fixed TestArea

--- src/structs/shapes_test.go
+++ src/structs/shapes_test.go
@@ -13,10 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
