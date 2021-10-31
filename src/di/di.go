d35df64628535cbdc2cf6fa85675e7de781af6d1
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 16:53:59 +0900

    add Greet function in main function

--- src/di/di.go
+++ src/di/di.go
@@ -3,8 +3,13 @@ package di
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(writer *bytes.Buffer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
