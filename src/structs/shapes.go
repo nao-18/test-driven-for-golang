fe683a313cc1181d914b039d7773725d586820b6
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-31 13:00:07 +0900

    add Shape interface

--- src/structs/shapes.go
+++ src/structs/shapes.go
@@ -11,6 +11,10 @@ type Circle struct {
 	Redius float64
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
