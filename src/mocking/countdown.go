d091f966c582c9c143533ec642559897fc82a99c
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-01 21:39:47 +0900

    fix test code and function

--- src/mocking/countdown.go
+++ src/mocking/countdown.go
@@ -10,45 +10,18 @@ type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
-type CountdownOperationsSpy struct {
-	Calls []string
-}
-
-func (s *CountdownOperationsSpy) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-const write = "write"
-const sleep = "sleep"
-
 const finalWord = "Go!"
-const constdownStart = 3
+const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := constdownStart; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
-	}
-
-	for i := constdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
 
--- src/mocking/countdown_test.go
+++ src/mocking/countdown_test.go
@@ -7,24 +7,21 @@ import (
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationsSpy{})
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
 	t.Run("sleep before every print", func(t *testing.T) {
 		spySleepPrinter := &CountdownOperationsSpy{}
@@ -46,3 +43,19 @@ Go!`
 		}
 	})
 }
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
