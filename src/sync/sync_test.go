042d1a0f13ded95df7ff007f6d04ee656a82d529
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-04 20:21:03 +0900

    add test code

--- src/sync/sync_test.go
+++ src/sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -14,6 +17,24 @@ func TestCounter(t *testing.T) {
 		if counter.Value() != 3 {
 			t.Errorf("got %d, want %d", counter.Value(), 3)
 		}
+	})
+
+	t.Run("it runs safely concuttently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
