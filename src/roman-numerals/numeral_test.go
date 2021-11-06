8903c84d37aded3a03c2b5e342fd4356c0dc567f
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-06 22:05:10 +0900

    fix to succeed

--- src/roman-numerals/numeral_test.go
+++ src/roman-numerals/numeral_test.go
@@ -121,7 +121,7 @@ func ConvertToArabic(roman string) int {
 				total += value
 				i++
 			} else {
-				total++
+				total += allRomanNumerals.ValueOf(symbol)
 			}
 		} else {
 			total += allRomanNumerals.ValueOf(symbol)
@@ -131,5 +131,6 @@ func ConvertToArabic(roman string) int {
 }
 
 func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
-	return index+1 < len(roman) && currentSymbol == 'I'
+	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+	return index+1 < len(roman) && isSubtractiveSymbol
 }
