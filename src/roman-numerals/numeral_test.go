d55010944cd605936d809ae23290e755e9bc45ce
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-06 20:54:44 +0900

    refactoring for test

--- src/roman-numerals/numeral_test.go
+++ src/roman-numerals/numeral_test.go
@@ -1,6 +1,9 @@
 package romannumerals
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 
@@ -26,11 +29,11 @@ func TestRomanNumerals(t *testing.T) {
 }
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 3 {
-		return "III"
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
 	}
-	if arabic == 2 {
-		return "II"
-	}
-	return "I"
+
+	return result.String()
 }
