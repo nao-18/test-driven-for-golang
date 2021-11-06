29de3cefaf91cc7845708d088533bf66fe90dd36
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-06 21:20:51 +0900

    refactoring for test code

--- src/roman-numerals/numeral_test.go
+++ src/roman-numerals/numeral_test.go
@@ -22,7 +22,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"8 gets converted to VIII", 8, "VIII"},
 		{"9 gets converted to IX", 9, "IX"},
 		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to IXV", 14, "IXV"},
+		{"14 gets converted to XIV", 14, "XIV"},
 		{"18 gets converted to XVIII", 18, "XVIII"},
 		{"20 gets converted to XX", 20, "XX"},
 		{"39 gets converted to XXXIX", 39, "XXXIX"},
@@ -39,27 +39,27 @@ func TestRomanNumerals(t *testing.T) {
 
 }
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 
-	if arabic == 4 {
-		return "IV"
-	}
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
