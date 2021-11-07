98322be3284c67848f8ef01e1e078e4a5f6a159d
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-07 10:35:59 +0900

    refactoring for test code

--- src/math/clockface/clockface_test.go
+++ src/math/clockface/clockface_test.go
@@ -26,3 +26,11 @@ func TestSecondsInRadians(t *testing.T) {
 		})
 	}
 }
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
