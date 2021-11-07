f3bc234c6dab7229d44f02496e31293eba593280
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-07 10:22:01 +0900

    add minimal code to make the test run

--- src/math/clockface/clockface_test.go
+++ src/math/clockface/clockface_test.go
@@ -1,11 +1,9 @@
-package clockface_test
+package clockface
 
 import (
 	"math"
 	"testing"
 	"time"
-
-	"github.com/nao-18/test-driven-for-golang/math/clockface"
 )
 
 func TestSecondsInRadians(t *testing.T) {
@@ -15,16 +13,5 @@ func TestSecondsInRadians(t *testing.T) {
 
 	if want != got {
 		t.Fatalf("Wanted %v radians, but got %v", want, got)
-	}
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
 	}
 }
