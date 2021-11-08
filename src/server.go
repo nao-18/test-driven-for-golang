e7ac9f1534a86239c83dec4731337a585c0ddb6c
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-08 21:52:14 +0900

    fixed to succeed

--- src/server.go
+++ src/server.go
@@ -17,6 +17,12 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
+
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	score := p.store.GetPlayerScore(player)
