6fa5d08be9ec39445afa129fd97c4389b8fd1f1c
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-08 09:52:47 +0900

    fixed to succeed

--- src/http-server/server.go
+++ src/http-server/server.go
@@ -1,5 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {}
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
+}
