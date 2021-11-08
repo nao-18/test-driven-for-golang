b90ba9cfd028e6aa22aff9cde675959bca644497
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-08 22:06:28 +0900

    refactoring for product code

--- src/hello.go
+++ src/hello.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"os"
-
-	"github.com/nao-18/test-driven-for-golang/di"
-	"github.com/nao-18/test-driven-for-golang/mocking"
 )
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+// InMemoryPlayerStore collects data about players in memory.
+type InMemoryPlayerStore struct{}
 
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	return greetingPrefix(language) + name
+// RecordWin will record a player's win.
+func (i *InMemoryPlayerStore) RecordWin(name string) {
 }
 
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-	return
+// GetPlayerScore retrieves scores for a given player.
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
 }
 
 func main() {
-	fmt.Println(Hello("world", ""))
-	sleeper := &mocking.DefaultSleeper{}
-	mocking.Countdown(os.Stdout, sleeper)
-	http.ListenAndServe(":9000", http.HandlerFunc(di.MyGreeterHandler))
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	if err := http.ListenAndServe(":9000", server); err != nil {
+		log.Fatalf("could not listen on port 9000 %v", err)
+	}
 }
