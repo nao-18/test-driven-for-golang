17a59a1b70319123d18d1d735bc4f4ff34da579f
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-08 21:22:44 +0900

    refactoring code

--- src/hello.go
+++ src/hello.go
@@ -3,51 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-
-	server "github.com/nao-18/test-driven-for-golang/http-server"
 )
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-	return
-}
-
+// InMemoryPlayerStore collects data about players in memory.
 type InMemoryPlayerStore struct{}
 
+// GetPlayerScore retrieves scores for a given player.
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
 }
 
 func main() {
-	// fmt.Println(Hello("world", ""))
-	// sleeper := &mocking.DefaultSleeper{}
-	// mocking.Countdown(os.Stdout, sleeper)
-	// http.ListenAndServe(":9000", http.HandlerFunc(di.MyGreeterHandler))
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 
-	// http-server
-	server := &server.PlayerServer{&InMemoryPlayerStore{}}
-	if err := http.ListenAndServe(":9000", server); err != nil {
-		log.Fatalf("could not listen on port 9000 %v", err)
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
--- src/server.go
+++ src/server.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"fmt"
@@ -6,15 +6,24 @@ import (
 	"strings"
 )
 
+// PlayerStore stores score information about players.
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 }
 
+// PlayerServer is a HTTP interface for player information.
 type PlayerServer struct {
 	store PlayerStore
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
 }
